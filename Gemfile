cd7ec55f66df882a36cf6ca8a1226c34a7cd7bcf
Matthew Landauer <mlandauer@gmail.com>
2019-07-10 13:03:01 +1000

    First pass at scraper

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
-# It's easy to add more libraries or choose different versions. Any libraries
-# specified here will be installed and made available to your morph.io scraper.
-# Find out more: https://morph.io/documentation/ruby
-
 source "https://rubygems.org"
 
-ruby "2.0.0"
+ruby "2.5.1"
 
+gem "horizon_xml", git: "https://github.com/planningalerts-scrapers/horizon_xml.git"
 gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
-gem "mechanize"
+
+gem "vcr"
+gem "webmock"
+gem "timecop"
--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,38 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
+Bundler.require
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+def scrape(authorities)
+  exceptions = {}
+  authorities.each do |authority_label|
+    puts "\nCollecting feed data for #{authority_label}..."
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    begin
+      HorizonXml.scrape(authority_label) do |record|
+        record["authority_label"] = authority_label.to_s
+        HorizonXml.log(record)
+        ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+      end
+    rescue StandardError => e
+      STDERR.puts "#{authority_label}: ERROR: #{e}"
+      STDERR.puts e.backtrace
+      exceptions[authority_label] = e
+    end
+  end
+  exceptions
+end
+
+authorities = HorizonXml::AUTHORITIES.keys
+puts "Scraping authorities: #{authorities.join(', ')}"
+exceptions = scrape(authorities)
+
+unless exceptions.empty?
+  puts "\n***************************************************"
+  puts "Now retrying authorities which earlier had failures"
+  puts "***************************************************"
+
+  exceptions = scrape(exceptions.keys)
+end
+
+unless exceptions.empty?
+  raise "There were errors with the following authorities: #{exceptions.keys}. See earlier output for details"
+end
